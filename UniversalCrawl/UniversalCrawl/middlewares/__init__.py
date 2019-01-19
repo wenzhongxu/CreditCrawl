2ed66e5b56c4576099601389ff31d03e3a2653b7
wenzhongxu <xuwenzhong1994@163.com>
2019-01-19 15:24:22 +0800

    增加代理池和IP地址池中间件

---
+++ UniversalCrawl/UniversalCrawl/middlewares/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/19 14:59
+# @Author  : XuWenzhong
+# @Email   : xuwenzhong1994@163.com
+# @File    : __init__.py.py
+# @Version : 1.0.1
+# @Software: PyCharm
